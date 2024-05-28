c0b9d2f81ec26d2c690f66253e9a56366d79ece9
Tobey14 <sureboytobi@gmail.com>
2024-05-29 00:40:18 +0100

    added properties

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,4 +48,9 @@ class User extends Authenticatable
     {
         return $this->hasOne(UserProfile::class);
     }
+
+    public function properties()
+    {
+        return $this->hasMany(Property::class);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\PropertyController;
 
 
 /*
@@ -30,6 +31,13 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/auth/logout', [UserController::class, 'logoutUser'])->name('logoutUser');
     Route::post('/user/profile', [UserController::class, 'updateUserProfile'])->name('updateUserProfile');
     Route::get('/user/profile', [UserController::class, 'getUserProfile'])->name('getUserProfile');
+
+    Route::post('/property', [PropertyController::class, 'createProperty'])->name('createProperty');
+    Route::get('/property/{id}', [PropertyController::class, 'getProperty'])->name('getProperty');
+    Route::get('/properties/all', [PropertyController::class, 'getAllProperty'])->name('getAllProperty');
+    Route::put('/property/{id}', [PropertyController::class, 'updateProperty'])->name('updateProperty');
+
+
     Route::post('/orders/paystack', [OrderController::class, 'paystackOrder'])->name('paystackOrder');
     Route::post('/orders/flutterwave/initiate', [OrderController::class, 'flutterwaveInitiate'])->name('flutterwaveInitiate');
 });
