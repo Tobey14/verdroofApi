4105c5cbded46899cd423a39270622266235bb3b
Tobey14 <sureboytobi@gmail.com>
2024-05-28 16:29:33 +0100

    authentication_done

---
+++ routes/api.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::post('/auth/login', [UserController::class, 'loginUser'])->name('loginUser');
+Route::post('/auth/create', [UserController::class, 'createUser'])->name('createUser');
+Route::post('/auth/verify/{token}', [UserController::class, 'verifyUser'])->name('verifyUser');
+
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::post('/auth/logout', [UserController::class, 'logoutUser'])->name('logoutUser');
+    Route::post('/user/profile', [UserController::class, 'updateUserProfile'])->name('updateUserProfile');
+    Route::get('/user/profile', [UserController::class, 'getUserProfile'])->name('getUserProfile');
+    Route::post('/orders/paystack', [OrderController::class, 'paystackOrder'])->name('paystackOrder');
+    Route::post('/orders/flutterwave/initiate', [OrderController::class, 'flutterwaveInitiate'])->name('flutterwaveInitiate');
+});
+
